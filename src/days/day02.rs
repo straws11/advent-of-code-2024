87e9aa40dd164d7539162774e8cf8ee7162dfbe7
straws11 <dswarts11@gmail.com>
2024-12-03 12:02:12 +0200

    Day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -4,7 +4,7 @@ use crate::utils;
 
 enum Safety {
     /// Idx to try and remove
-    Unsafe(usize),
+    Unsafe(Vec<usize>),
     Safe,
 }
 
@@ -26,20 +26,37 @@ pub fn run() {
 pub fn day2(input: Vec<String>, damping: bool) -> usize {
     let mut count: usize = 0;
     for report in &input {
-        let mut rep = report
+        let rep = report
             .split(" ")
             .map(|s| s.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
         match determine_safe(&rep) {
             Safety::Safe => count += 1,
-            Safety::Unsafe(try_idx) => {
+            Safety::Unsafe(try_indices) => {
                 if !damping {
                     continue;
                 }
-                rep.remove(try_idx);
-                if let Safety::Safe = determine_safe(&rep) {
-                    count += 1;
+                println!("Before: {:?}", rep);
+                for i in 0..rep.len() {
+                    let mut rep_clone = rep.clone();
+                    rep_clone.remove(i);
+                    println!("After: {:?}", rep_clone);
+                    if let Safety::Safe = determine_safe(&rep_clone) {
+                        count += 1;
+                        println!("Safe");
+                        break;
+                    }
                 }
+                /* for idx in try_indices {
+                    let mut rep_clone = rep.clone();
+                    rep_clone.remove(idx);
+                    println!("After: {:?}", rep_clone);
+                    if let Safety::Safe = determine_safe(&rep_clone) {
+                        count += 1;
+                        println!("Safe");
+                        break;
+                    }
+                } */
             }
         }
     }
@@ -49,17 +66,17 @@ pub fn day2(input: Vec<String>, damping: bool) -> usize {
 fn determine_safe(rep: &[i32]) -> Safety {
     // not inc or decreasing
     if rep[0] == rep[1] {
-        return Safety::Unsafe(1);
+        return Safety::Unsafe([0, 1].to_vec());
     }
     let increasing: bool = rep[1] > rep[0];
     for i in 1..rep.len() {
         let diff = rep[i] - rep[i - 1];
 
         if !(1..=3).contains(&diff.abs()) {
-            return Safety::Unsafe(i);
+            return Safety::Unsafe([i, i - 1].to_vec());
         }
         if (increasing && (diff < 0)) || (!increasing && (diff > 0)) {
-            return Safety::Unsafe(i);
+            return Safety::Unsafe([i, i - 1].to_vec());
         }
     }
     Safety::Safe
@@ -93,5 +110,11 @@ mod tests {
             "1 3 6 7 9".to_string(),
         ];
         assert_eq!(day2(input, true), 4);
+    }
+
+    #[test]
+    fn test_edge() {
+        let input = ["12 11 14 17".to_string()].to_vec();
+        assert_eq!(day2(input, true), 1);
     }
 }
